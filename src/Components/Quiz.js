6841597de82d169fc0b4af6c8dd132f9b94bcad6
Palash Deka <palashjyoti12@gmail.com>
2024-02-19 00:06:58 +0530

    first commit

---
+++ src/Components/Quiz.js
@@ -0,0 +1,70 @@
+import React, { useRef, useState } from 'react'
+import './quiz.css'
+import { data } from '../Questions/data'
+
+const Quiz = () => {
+    let [index,setIndex]= useState(0);
+    let [question,setQuestion]= useState(data[index]);
+    let [lock,setLock] = useState(false)
+    let [score,setScore]=useState(0)
+    let [result,setResult]=useState(false)
+    let Option1 = useRef(null);
+    let Option2 = useRef(null);
+    let Option3 = useRef(null);
+    let Option4 = useRef(null);
+
+    let option_array=[Option1,Option2,Option3,Option4]
+    const checkAns =(e, ans)=>{
+      if(lock===false) {
+        if (question.ans===ans){
+      e.target.classList.add('correct')
+      setLock(true);
+      setScore(prev=>prev+1)
+     }
+      else{
+      e.target.classList.add('incorrect')
+      setLock(true)
+      option_array[question.ans-1].current.classList.add('correct')
+     }
+      }
+     
+    }
+
+    const next=()=>{
+      if (lock===true) {
+        if(index === data.length-1){
+          setResult(true);
+          return 0;
+        }
+        setIndex(++index)
+        setQuestion(data[index]);
+        setLock(false);
+        option_array.map((option)=>{
+          option.current.classList.remove('incorrect');
+          option.current.classList.remove('correct');
+          return null;
+          
+        })
+      }
+    }
+  return (
+    <div className='container'>
+      <h1>Quiz App</h1>
+      <hr />
+      {result?<></>:<> 
+      <h2>{index+1}.{question.question}</h2>
+      
+      <ul>
+        <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+        <li ref={Option1} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+        <li ref={Option1} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+        <li ref={Option1} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+      </ul>
+      <button onClick={next}>next</button>
+      <div className="index"> {index+1} of {data.length}question</div></>}
+      <h2> score is {score} out of {data.length}</h2>
+    </div>
+  )
+}
+
+export default Quiz
